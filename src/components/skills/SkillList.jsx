5efae4aae31096e614f0e5acf93a4b0511408957
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-26 14:47:03 +0700

    Show skills based on primary property

--- src/components/skills/SkillList.jsx
+++ src/components/skills/SkillList.jsx
@@ -4,14 +4,30 @@ import PropTypes from 'prop-types';
 import SkillItem from './SkillItem';
 
 function SkillList({ skills }) {
+  const primarySkills = skills.filter((skill) => skill.primary === true);
+  const secondarySkills = skills.filter((skill) => skill.primary === false);
+  const primaryStyles = {
+    icon: 'icon-md',
+    iconSvg: 'icon-svg-md',
+  };
+  const secondaryStyles = {
+    icon: 'icon-sm',
+    iconSvg: 'icon-svg-sm',
+  };
+
   return (
     <section className="skills" id="skills">
       <h2 className="skill-header">My Skills</h2>
       <div className="skills-wrapper">
         <div className="first-set animate__animated animate__pulse">
           <div className="skills-container">
-            {skills.map((skill) => (
-              <SkillItem key={skill.id} {...skill} />
+            {primarySkills.map((skill) => (
+              <SkillItem key={skill.id} style={primaryStyles} {...skill} />
+            ))}
+          </div>
+          <div className="skills-container">
+            {secondarySkills.map((skill) => (
+              <SkillItem key={skill.id} style={secondaryStyles} {...skill} />
             ))}
           </div>
         </div>
