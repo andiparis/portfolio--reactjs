5106bfe46da0436fb760644ff3c34a701ac5c6f0
Andi Paris Bachtiar <andiparis02@gmail.com>
2024-02-29 19:15:20 +0700

    Update the data

--- src/utils/data.js
+++ src/utils/data.js
@@ -67,53 +67,53 @@ const getProjectsData = () => {
     {
       id: 1,
       name: 'Sistem Informasi Akutansi',
-      tech: 'CodeIgniter - STISLA - JavaScript',
+      tech: ['CodeIgniter', 'STISLA', 'JavaScript'],
       description:
         'The accounting application as the name suggests, provides features that can help manage accounting transactions. Furthermore, in this application you can print every existing report.',
       github: 'https://github.com/andiparis/accountant-app--codeigniter4',
-      deploy: null,
+      demo: null,
       img: '/assets/images/p-akutansi.jpg',
     },
     {
       id: 2,
       name: 'Know Your Weather',
-      tech: 'ReactJS - Material UI - API',
+      tech: ['ReactJS', 'MUI', 'REST API'],
       description:
         'This application provides real-time weather forecast information from openweather API. This application was built together with my friends.',
       github: 'https://github.com/andiparis/know-your-weather--development',
-      deploy: 'https://knowyourweather-app.netlify.app',
+      demo: 'https://knowyourweather-app.netlify.app',
       img: '/assets/images/p-know-your-weather.jpg',
     },
     {
       id: 3,
       name: 'MyCasir',
-      tech: 'CodeIgniter - AdminLTE - jQuery',
+      tech: ['CodeIgniter', 'AdminLTE', 'jQuery'],
       description:
         ' This application can help in managing sales transactions in a shop. In the application, several features are available for managing merchandise data and of course the sales transactions themselves.',
       github: 'https://github.com/andiparis/mycasir2--codeigniter3',
-      deploy: null,
+      demo: null,
       img: '/assets/images/p-pos.jpg',
     },
-    {
-      id: 4,
-      name: 'InResto',
-      tech: 'JavaScript - PWA - API',
-      description:
-        'This application displays a variety of restaurant data available via the Dicoding API. This application implements PWA, making it very user-friendly when used on mobile devices.',
-      github: 'https://github.com/andiparis/in-resto--javascript',
-      deploy: 'https://indonesiarestaurant.netlify.app',
-      img: '/assets/images/p-inresto.jpg',
-    },
-    {
-      id: 5,
-      name: 'SIP-BSM',
-      tech: 'CodeIgniter - AdminLTE - Bootstrap',
-      description:
-        'This application is my TA project. In this application, you can manage your own training activities or training requests from the community.',
-      github: 'https://github.com/andiparis/sip-bsm--codeigniter3',
-      deploy: null,
-      img: '/assets/images/p-sip-bsm.jpg',
-    },
+    // {
+    //   id: 4,
+    //   name: 'InResto',
+    //   tech: 'JavaScript - PWA - API',
+    //   description:
+    //     'This application displays a variety of restaurant data available via the Dicoding API. This application implements PWA, making it very user-friendly when used on mobile devices.',
+    //   github: 'https://github.com/andiparis/in-resto--javascript',
+    //   demo: 'https://indonesiarestaurant.netlify.app',
+    //   img: '/assets/images/p-inresto.jpg',
+    // },
+    // {
+    //   id: 5,
+    //   name: 'SIP-BSM',
+    //   tech: 'CodeIgniter - AdminLTE - Bootstrap',
+    //   description:
+    //     'This application is my TA project. In this application, you can manage your own training activities or training requests from the community.',
+    //   github: 'https://github.com/andiparis/sip-bsm--codeigniter3',
+    //   demo: null,
+    //   img: '/assets/images/p-sip-bsm.jpg',
+    // },
   ];
 };
 
